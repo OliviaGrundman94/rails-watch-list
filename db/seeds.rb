596ee38cc7d557759b1338e1d7925f8075a000a0
Olivia Grundman <oliviagrundman@gmail.com>
2022-08-25 16:22:20 +0100

    routing controller and views for lists - also created seeds to add faker info

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,25 @@
 # Examples:
 #
 require 'faker'
-
-
+Movie.destroy_all
 puts "Creating database"
 
 10.times do
-  movie = Movie.create(
+  movie = Movie.create!(
     title: Faker::Movie.title,
-    overview: Faker::Movie.quote,
+    overview: Faker::Quote.famous_last_words,
     poster_url: Faker::LoremFlickr.image,
     rating: rand(1..5)
   )
   puts "Movie with id: #{movie.id} has been created"
 end
 
-puts "Finished!"
+puts "Finished movies!"
+
+3.times do
+  list = List.create(
+    name: Faker::Name.name
+  )
+  puts "Lists with id: #{list.id} has been created"
+end
+puts "Finished lists!"
